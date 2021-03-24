054c81833bf8f29b3b15c925bb1051f46cf4ed87
dyusupov <dmitry.yusupov@infobip.com>
2021-03-24 13:04:46 +0300

    Fixed gradle scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,16 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.4.4"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.4.31"
-    kotlin("plugin.spring") version "1.4.31"
+    id("org.springframework.boot") version "2.4.4" apply false
+    id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
+    kotlin("jvm") version "1.4.31" apply false
+    kotlin("plugin.spring") version "1.4.31" apply false
+
+    kotlin("plugin.allopen") version "1.3.61" apply false
+    kotlin("plugin.jpa") version "1.4.10" apply false
 }
 
-java.sourceCompatibility = JavaVersion.VERSION_14
+//java.sourceCompatibility = JavaVersion.VERSION_14
 
 repositories {
     mavenCentral()
@@ -41,15 +44,39 @@ allprojects {
 }
 
 subprojects {
-    apply(plugin = "org.jetbrains.kotlin.jvm")
 
-    dependencies {
-        implementation("org.springframework.boot", "spring-boot-starter")
-        implementation("org.jetbrains.kotlin:kotlin-reflect")
-        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+   /* apply {
+        plugin("io.spring.dependency-management")
+        plugin("org.jetbrains.kotlin.jvm")
+        plugin("org.jetbrains.kotlin.plugin.spring")
+    }*/
 
-        testImplementation("org.springframework.boot", "spring-boot-starter-test") {
-            exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
-        }
+    repositories {
+        mavenCentral()
     }
+
+    apply {
+        plugin("io.spring.dependency-management")
+    }
+
+   /* dependencyManagement {
+        val bootVersion: String by project
+        imports {
+            mavenBom("org.springframework.boot:spring-boot-dependencies:$bootVersion")
+        }
+    }*/
+
+//    apply(plugin = "org.jetbrains.kotlin.jvm")
+//    apply(plugin = "org.springframework.boot")
+//    apply(plugin = "plugin.spring")
+
+//    dependencies {
+//        implementation("org.springframework.boot", "spring-boot-starter")
+//        implementation("org.jetbrains.kotlin:kotlin-reflect")
+//        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+//
+//        testImplementation("org.springframework.boot", "spring-boot-starter-test") {
+//            exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+//        }
+//    }
 }
--- jdbc/build.gradle.kts
+++ jdbc/build.gradle.kts
@@ -1,18 +1,18 @@
 plugins {
     id("org.springframework.boot")
-    id("io.spring.dependency-management")
+
     kotlin("jvm")
     kotlin("plugin.spring")
 
-//    kotlin("plugin.allopen") version "1.3.61"
-//    kotlin("plugin.jpa") version "1.4.10"
+    kotlin("plugin.allopen")
+    kotlin("plugin.jpa")
 }
-/*
+
 allOpen {
     annotation("javax.persistence.Entity")
     annotation("javax.persistence.Embeddable")
     annotation("javax.persistence.MappedSuperclass")
-}*/
+}
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
