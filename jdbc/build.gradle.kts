869c30f617b2e9cb6f871e3b65c0b128ddc85223
dyusupov <dmitry.yusupov@infobip.com>
2021-03-24 14:08:08 +0300

    Included prometheuws and graphana docker compose files

--- jdbc/build.gradle.kts
+++ jdbc/build.gradle.kts
@@ -15,6 +15,20 @@ allOpen {
 }
 
 dependencies {
+    implementation("io.micrometer:micrometer-registry-prometheus")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+
+    implementation("org.springframework.boot", "spring-boot-starter")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    runtimeOnly("com.h2database:h2")
+
+    testImplementation("org.springframework.boot", "spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
+
 }
