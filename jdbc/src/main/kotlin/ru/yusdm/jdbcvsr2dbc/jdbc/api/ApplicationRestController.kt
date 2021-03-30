623b837b027d94f1039359f80a1c0680bfe3f0ae
dmitry <usikovich2@gmail.com>
2021-03-30 15:44:26 +0300

    Added jmeter dashboard, removed logging

--- jdbc/src/main/kotlin/ru/yusdm/jdbcvsr2dbc/jdbc/api/ApplicationRestController.kt
+++ jdbc/src/main/kotlin/ru/yusdm/jdbcvsr2dbc/jdbc/api/ApplicationRestController.kt
@@ -1,14 +1,11 @@
 package ru.yusdm.jdbcvsr2dbc.jdbc.api
 
 import org.springframework.web.bind.annotation.*
-import ru.yusdm.jdbcvsr2dbc.common.dto.CountryDTO
 import ru.yusdm.jdbcvsr2dbc.jdbc.domain.City
 import ru.yusdm.jdbcvsr2dbc.jdbc.domain.Country
-import ru.yusdm.jdbcvsr2dbc.jdbc.domain.toDTO
 import ru.yusdm.jdbcvsr2dbc.jdbc.service.CityService
 import ru.yusdm.jdbcvsr2dbc.jdbc.service.CountryService
 import java.util.*
-import java.util.concurrent.atomic.AtomicInteger
 
 @RestController
 @RequestMapping(value = ["/api"])
@@ -16,8 +13,6 @@ class ApplicationRestController(
     private val countryService: CountryService,
     private val cityService: CityService
 ) {
-
-    private val counter = AtomicInteger(0)
 
     @GetMapping("/cities/{cityId}")
     fun getCity(@PathVariable("cityId") cityId: UUID): City {
@@ -46,8 +41,6 @@ class ApplicationRestController(
 
     @GetMapping("/countries")
     fun getCountries(@RequestParam("fetch_cities") fetchCities: Boolean): List<Country> {
-        val c = counter.incrementAndGet()
-        println("COUNTER " + c)
         return countryService.findAllCountries()
     }
 
